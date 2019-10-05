a7c509c89b03ba89ebd33a5b5ad5a351cea217e9
Nicolas Le Hir <nico@iMac-de-Nicolas.local>
2019-10-05 18:12:26 +0200

    First commit

---
+++ random_inspector.py
@@ -0,0 +1,85 @@
+import socket
+import os
+import logging
+from logging.handlers import RotatingFileHandler
+import json
+import protocol
+from random import randrange
+import random
+
+host = "localhost"
+port = 12000
+# HEADERSIZE = 10
+
+"""
+set up inspector logging
+"""
+inspector_logger = logging.getLogger()
+inspector_logger.setLevel(logging.DEBUG)
+formatter = logging.Formatter(
+    "%(asctime)s :: %(levelname)s :: %(message)s", "%H:%M:%S")
+# file
+if os.path.exists("./logs/inspector.log"):
+    os.remove("./logs/inspector.log")
+file_handler = RotatingFileHandler('./logs/inspector.log', 'a', 1000000, 1)
+file_handler.setLevel(logging.DEBUG)
+file_handler.setFormatter(formatter)
+inspector_logger.addHandler(file_handler)
+# stream
+stream_handler = logging.StreamHandler()
+stream_handler.setLevel(logging.WARNING)
+inspector_logger.addHandler(stream_handler)
+
+
+class Player():
+
+    def __init__(self):
+
+        self.end = False
+        # self.old_question = ""
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+    def connect(self):
+        self.socket.connect((host, port))
+
+    def reset(self):
+        self.socket.close()
+
+    def answer(self, question):
+        # work
+        data = question["data"]
+        game_state = question["game state"]
+        response_index = random.randint(0, len(data)-1)
+        # log
+        inspector_logger.debug("|\n|")
+        inspector_logger.debug("inspector answers")
+        inspector_logger.debug(f"question type ----- {question['question type']}")
+        inspector_logger.debug(f"data -------------- {data}")
+        inspector_logger.debug(f"response index ---- {response_index}")
+        inspector_logger.debug(f"response ---------- {data[response_index]}")
+        return response_index
+
+    def handle_json(self, data):
+        data = json.loads(data)
+        response = self.answer(data)
+        # send back to server
+        bytes_data = json.dumps(response).encode("utf-8")
+        protocol.send_json(self.socket, bytes_data)
+
+    def run(self):
+
+        self.connect()
+
+        while self.end is not True:
+            received_message = protocol.receive_json(self.socket)
+            if received_message:
+                self.handle_json(received_message)
+            else:
+                print("no message, finished learning")
+                self.end = True
+
+
+p = Player()
+
+p.run()
