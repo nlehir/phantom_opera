eb0d82c3b7717e5b83fda4f7b824c0b5fd357cf7
PierreBougon <pierre.bougon@epitech.eu>
2020-09-06 23:23:27 +0200

    fix error handling in client

--- src/network/Client.py
+++ src/network/Client.py
@@ -60,8 +60,8 @@ class Client:
                         self.disconnect()
                 except socket.error as e:
                     if isinstance(e.args, tuple):
-                        self.logger.error("errno error")
-                        if e[0] == errno.EPIPE:
+                        self.logger.error("errno error: ", e.args[0])
+                        if e.args[0] == errno.EPIPE:
                             # remote peer disconnected
                             self.logger.error("Detected remote disconnect")
                         else:
